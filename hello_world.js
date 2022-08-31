b74fea1d9eabc33220d497a731efef01c6530b94
1oopr0 <1oo.ooooo.pro@gmail.com>
2022-08-31 23:00:12 +0900

    component and React.Component

--- hello_world.js
+++ hello_world.js
@@ -1,22 +1,122 @@
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-function tick() {
-    var element = React.createElement(
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+// [함수 사용]
+// function tick(){
+//     const element = (
+//         <div>
+//             <h1>Hello, world!</h1>
+//             <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//         </div>
+//     );
+//     ReactDOM.render(element, document.getElementById('root'));
+// }
+
+// [함수 컴포넌트 사용]
+// function Clock(props) {
+//     return (
+//         <div>
+//             <h1>Hello, world!</h1>
+//             <h2>It is {props.date.toLocaleTimeString()}.</h2>
+//         </div>
+//     );
+// }
+
+// // 아래는 Clock에 props를 전달하지 않고 구현하는 것이 좋다.
+// function tick(){
+//     const element = <Clock date={new Date()} />;
+//     ReactDOM.render(element, document.getElementById('root'));
+// }
+
+
+// [Class 컴포넌트로 변경 후, state 와 생명주기 사용하기]
+var Clock = function (_React$Component) {
+    _inherits(Clock, _React$Component);
+
+    // state 설정하기
+    function Clock(props) {
+        _classCallCheck(this, Clock);
+
+        var _this = _possibleConstructorReturn(this, (Clock.__proto__ || Object.getPrototypeOf(Clock)).call(this, props));
+
+        _this.state = { date: new Date() };
+        return _this;
+    }
+
+    //#region 생명주기 메서드 사용
+
+    // 컴포넌트 출력물이 DOM에 렌더링 된 후에 실행된다.
+
+
+    _createClass(Clock, [{
+        key: 'componentDidMount',
+        value: function componentDidMount() {
+            var _this2 = this;
+
+            this.timerID = setInterval(function () {
+                return _this2.tick();
+            }, 1000);
+        }
+
+        // 컴포넌트가 렌더링 해제될 때 실행된다.
+
+    }, {
+        key: 'componentWillUnmount',
+        value: function componentWillUnmount() {
+            clearInterval(this.timerID);
+        }
+        //#endregion
+
+        // setState 함수를 사용하여 state를 재설정하기
+
+    }, {
+        key: 'tick',
+        value: function tick() {
+            this.setState({
+                date: new Date()
+            });
+        }
+    }, {
+        key: 'render',
+        value: function render() {
+            return React.createElement(
+                'div',
+                null,
+                React.createElement(
+                    'h1',
+                    null,
+                    'Hello, world!'
+                ),
+                React.createElement(
+                    'h2',
+                    null,
+                    'It is ',
+                    this.state.date.toLocaleTimeString(),
+                    '.'
+                )
+            );
+        }
+    }]);
+
+    return Clock;
+}(React.Component);
+
+// 컴포넌트는 각각 독립적으로 사용된다.
+
+
+function App() {
+    return React.createElement(
         'div',
         null,
-        React.createElement(
-            'h1',
-            null,
-            'Hello, world!'
-        ),
-        React.createElement(
-            'h2',
-            null,
-            'It is ',
-            new Date().toLocaleTimeString(),
-            '.'
-        )
+        React.createElement(Clock, null),
+        React.createElement(Clock, null),
+        React.createElement(Clock, null)
     );
-    ReactDOM.render(element, document.getElementById('root'));
 }
 
-setInterval(tick, 1000);
+ReactDOM.render(React.createElement(App, null), document.getElementById('root'));
--- src/hello_world.js
+++ src/hello_world.js
@@ -1,13 +1,81 @@
+// [함수 사용]
+// function tick(){
+//     const element = (
+//         <div>
+//             <h1>Hello, world!</h1>
+//             <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//         </div>
+//     );
+//     ReactDOM.render(element, document.getElementById('root'));
+// }
 
-function tick(){
-    const element = (
-        <div>
-            <h1>Hello, world!</h1>
-            <h2>It is {new Date().toLocaleTimeString()}.</h2>
-        </div>
-    );
-    ReactDOM.render(element, document.getElementById('root'));
+// [함수 컴포넌트 사용]
+// function Clock(props) {
+//     return (
+//         <div>
+//             <h1>Hello, world!</h1>
+//             <h2>It is {props.date.toLocaleTimeString()}.</h2>
+//         </div>
+//     );
+// }
+
+// // 아래는 Clock에 props를 전달하지 않고 구현하는 것이 좋다.
+// function tick(){
+//     const element = <Clock date={new Date()} />;
+//     ReactDOM.render(element, document.getElementById('root'));
+// }
+
+
+// [Class 컴포넌트로 변경 후, state 와 생명주기 사용하기]
+class Clock extends React.Component {
+    // state 설정하기
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+    }
+
+    //#region 생명주기 메서드 사용
+
+    // 컴포넌트 출력물이 DOM에 렌더링 된 후에 실행된다.
+    componentDidMount() {
+        this.timerID = setInterval(()=>this.tick(), 1000);
+    }
+
+    // 컴포넌트가 렌더링 해제될 때 실행된다.
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+    //#endregion
+
+    // setState 함수를 사용하여 state를 재설정하기
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render(){
+        return(
+            <div>
+                 <h1>Hello, world!</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+            </div>
+        );
+    }
 }
 
-setInterval(tick, 1000);
+// 컴포넌트는 각각 독립적으로 사용된다.
+function App() {
+    return (
+        <div>
+            <Clock />
+            <Clock />
+            <Clock />
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
 
